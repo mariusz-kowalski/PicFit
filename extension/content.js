3fddfb2b69debc9bce6bf6f3f96136ccf6acafd0
Mariusz Kowalski <mariusz.kowalski@eniat.com>
2025-06-23 11:59:15 +0200

    it works

--- extension/content.js
+++ extension/content.js
@@ -1,14 +1,27 @@
-if (document.contentType && document.contentType.startsWith('image/')) {
-	const bodyChildren = document.body.children
-	if (bodyChildren.length === 1 && bodyChildren[0].tagName === 'IMG') {
-		document.documentElement.style.height = '100%'
-		document.body.style.margin = '0'
-		document.body.style.display = 'flex'
-		document.body.style.justifyContent = 'center'
-		document.body.style.alignItems = 'center'
-		const img = bodyChildren[0]
-		img.style.maxWidth = '100vw'
-		img.style.maxHeight = '100vh'
-		img.style.objectFit = 'contain'
+function fitImageToViewport() {
+	if(document.contentType && document.contentType.startsWith('image/')) {
+		const bodyChildren = document.body.children
+		if(bodyChildren.length === 1 && bodyChildren[0].tagName === 'IMG') {
+			const img = bodyChildren[0]
+
+			const window_ratio = window.innerWidth / window.innerHeight
+			const img_ratio = img.naturalWidth / img.naturalHeight
+
+			if(img_ratio > window_ratio) {
+				img.style.width = '100%'
+				img.style.height = 'auto'
+			} else {
+				img.style.width = 'auto'
+				img.style.height = '100%'
+			}
+		}
 	}
 }
+
+fitImageToViewport()
+
+let resizeTimeout
+window.addEventListener('resize', () => {
+	clearTimeout(resizeTimeout)
+	resizeTimeout = setTimeout(fitImageToViewport, 100)
+})
